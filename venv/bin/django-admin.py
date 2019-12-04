4c9196456b098d1b2610c792c8ee6bec4ca877ae
Thomas Gaylard Admin <tbg-beanstalk@genonline.co.uk>
2019-12-04 22:36:23 +0000

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/thomasdeveloper/Documents/Projects/outlines_unleashed_webapp/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
